4c8048e1574de7a33b2c952c7ba3c48621230f55
bacpv3011 <bac.pv172964@sis.hust.edu.vn>
2020-12-21 16:57:12 +0700

    Add

--- EcoBike/Lab05-OMSVirtualServer/src/com/oms/db/IMediaDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.oms.db;
-
-import java.util.ArrayList;
-
-import com.oms.bean.Station;
-
-public interface IMediaDatabase {
-	public ArrayList<Station> searchStation(Station station);
-}
