f9ef6002cfcece0c6a1d675a839f2b7ce11f8e2a
bacpv3011 <bac.pv172964@sis.hust.edu.vn>
2020-12-21 16:55:34 +0700

    add

---
+++ EcoBike/src/api/EcoApi.java
@@ -0,0 +1,45 @@
+package api;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.eco.bean.Station;
+
+public class EcoApi {
+	public static final String PATH = "http://localhost:8080/";
+	
+	private Client client;
+	
+	public EcoApi() {
+		client = ClientBuilder.newClient();
+	}
+	
+	public ArrayList<Station> getStations(Map<String, String> queryParams) {
+		WebTarget webTarget = client.target(PATH).path("stations");
+		
+		
+		if (queryParams != null) {
+			for (String key : queryParams.keySet()) {
+				String value = queryParams.get(key);
+				webTarget = webTarget.queryParam(key, value);
+			}
+		}
+		
+
+		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+		Response response = invocationBuilder.get();
+		ArrayList<Station> res = response.readEntity(new GenericType<ArrayList<Station>>() {});
+		System.out.println(res);
+		return res;
+	}
+}
---
+++ EcoServer/src/com/oms/bean/Station.java
@@ -0,0 +1,92 @@
+package com.oms.bean;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonTypeName("station")
+public class Station {
+	private String id;
+	
+	private String title;
+
+	private String name;
+	private String address;
+	
+	public Station() {
+		super();
+	}
+
+	public Station(String id, String title, String name, String address) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.name = name;
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "id: " + this.id + ", title: " + this.title + ", name: " + this.name + ", address: " + this.address;
+	}
+	
+	
+	public boolean match(Station media) {
+		if (media == null)
+			return true;
+		
+		
+		if (media.id != null && !media.id.equals("") && !this.id.contains(media.id)) {
+			return false;
+		}
+		if (media.title != null && !media.title.equals("") && !this.title.contains(media.title)) {
+			return false;
+		}
+		if (media.name != null && !media.name.equals("") && !this.name.contains(media.name)) {
+			return false;
+		}
+		if (media.address != null && !media.address.equals("") && !this.address.contains(media.address)) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Station) {
+			return this.id.equals(((Station) obj).id);
+		}
+		return false;
+	}
+}
